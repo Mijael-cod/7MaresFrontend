80073819b44285b1a66f7c454d46cd8e9aebfa9f
MijaelR26 <82177948+Mijael-cod@users.noreply.github.com>
2023-05-29 23:34:06 -0500

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistrarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
