13e043e8af2ec33f3504de8b5769c993b4b2031f
MijaelR26 <82177948+Mijael-cod@users.noreply.github.com>
2023-06-09 23:35:14 -0500

    new

--- src/app/pages/mesero/mesero-routing.module.ts
+++ src/app/pages/mesero/mesero-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { PlatilloComponent } from './platillo/platillo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categoria', component:CategoriaComponent, title: "Categoria"},
+  {path: 'platillo', component:PlatilloComponent, title: "Platillo"}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { MeseroModule } from './mesero/mesero.module';
 
 
 @NgModule({
   declarations: [
   ],
   imports: [
+    //Aca se importa los modulos de cada rol (Admin, Cajero, Cocinero, Mesero, Vistas)
     CommonModule,
     PagesRoutingModule,
     SharedModule,
-    
+    MeseroModule
   ]
 })
 export class PagesModule { }
