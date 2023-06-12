d44baf9be71c0bf5aedc6be68c263815372037cd
Miguel Trejo <miguel.trejo@upeu.edu.pe>
2023-06-12 00:51:36 -0500

    menu 9

--- src/app/pages/mesero/categoria/categoria.component.spec.ts
+++ src/app/pages/mesero/categoria/categoria.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { CategoriaComponent } from './categoria.component';
+
 
 describe('CategoriaComponent', () => {
   let component: CategoriaComponent;
@@ -20,4 +22,8 @@ describe('CategoriaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
+
+
+
--- src/app/pages/mesero/categoria/categoria.component.ts
+++ src/app/pages/mesero/categoria/categoria.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.css']
 })
+
+
 export class CategoriaComponent implements OnInit {
 
   constructor() { }
