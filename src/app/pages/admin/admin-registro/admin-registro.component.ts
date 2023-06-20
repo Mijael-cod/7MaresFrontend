8d3b165400114adc0facd3e439b84a4d1774a9d0
Miguel Trejo <miguel.trejo@upeu.edu.pe>
2023-06-20 16:44:41 -0500

    registrossss

---
+++ src/app/pages/admin/admin-registro/admin-registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-registro',
+  templateUrl: './admin-registro.component.html',
+  styleUrls: ['./admin-registro.component.css']
+})
+export class AdminRegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/mesero/categoria/categoria.component.ts
+++ src/app/pages/mesero/categoria/categoria.component.ts
@@ -97,3 +97,4 @@ export class CategoriaComponent implements OnInit {
   }
 
 }
+
