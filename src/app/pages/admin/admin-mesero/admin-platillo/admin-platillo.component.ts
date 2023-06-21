7e03de9258b4781417ff619adedf64ed369330ac
MatSiDX <brayangbh478@gmail.com>
2023-06-21 10:21:07 -0500

    Funcionalidades mesero platillo y admin

--- src/app/pages/admin/admin-mesero/admin-platillo/admin-platillo.component.ts
+++ src/app/pages/admin/admin-mesero/admin-platillo/admin-platillo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServiceResponse } from 'src/app/interfaces/ServiceResponse .interface';
+import { Platillos } from 'src/app/models/Platillos.model';
 import { PlatillosDto } from 'src/app/models/PlatillosDto.model';
 import { MeseroService } from 'src/app/services/mesero.service';
 
@@ -11,7 +12,9 @@ import { MeseroService } from 'src/app/services/mesero.service';
 })
 export class AdminPlatilloComponent implements OnInit {
   
+  platillos: Platillos[] = [];
   platillosDto: PlatillosDto[] = [];
+  platilloDto: PlatillosDto = new PlatillosDto();
 
   constructor(private router: Router, private meseroService: MeseroService) { }
 
@@ -21,16 +24,69 @@ export class AdminPlatilloComponent implements OnInit {
 
   listarPlatillosPorIdDeCategoria() {
     const idCategoria = localStorage.getItem('idCategoria');
-    this.meseroService
-      .listarPlatillosPorIdCategoria(+idCategoria)
+    this.meseroService.listarPlatillosPorIdCategoria(+idCategoria)
       .subscribe(
         {
           next: (serviceResponse: ServiceResponse) => {
             this.platillosDto = serviceResponse.data
             console.log(this.platillosDto);
-          }
+          },
+
         }
       );
+  }
+
+  agregarPlatillo(event: Event) {
+    const idCategoria = localStorage.getItem('idCategoria');
+    this.platilloDto.categorias = +idCategoria;
+    this.meseroService.agregarPlatillos(this.platilloDto).subscribe({
+      next: (res: any) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+    window.location.reload();
+  }
+
+  editarPlatillo(event: Event) {
+    const id = localStorage.getItem("idPlat")
+    console.log("entrando");
+    this.meseroService
+      .editarPlatillo(this.platilloDto, +id)
+      .subscribe(
+        {
+          next: (resp: any) => {
+            console.log(resp);
+            localStorage.removeItem('idPlat');
+            this.router.navigate(['/admin-panel/admin-platillo'])
+          }, error: (err) => {
+            console.log(err);
+          }
+        })
+    window.location.reload();
+  }
+
+  eliminarPlatillo() {
+    const id = localStorage.getItem("idPlat")
+
+    this.meseroService
+      .eliminarPlatillo(+id)
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          localStorage.removeItem('idPlat');
+          this.router.navigate(['/admin-panel/admin-platillo'])
+        }, error: (err) => {
+          console.log(err);
+        }
+      })
+    window.location.reload();
+  }
+
+  subirIdPlatillo(idPlat: number) {
+    localStorage.setItem('idPlat', idPlat.toString())
   }
 
   volverAdminCategoria() {
