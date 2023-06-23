07458b392e741f73e3a357f3230fcf82609b2363
Miguel Trejo <miguel.trejo@upeu.edu.pe>
2023-06-22 23:32:24 -0500

    menu 292992

--- src/app/pages/vistas/v-principal/v-principal.component.ts
+++ src/app/pages/vistas/v-principal/v-principal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-v-principal',
@@ -7,12 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VPrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  irLogin() {
+    this.router.navigate(['/login'])
+  }
+
+  
+  seccion1() {
+    const seccion1 = this.elementRef.nativeElement.querySelector('#s1');
+    seccion1.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  seccion2() {
+    const seccion1 = this.elementRef.nativeElement.querySelector('#s2');
+    seccion1.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  seccion3() {
+    const seccion1 = this.elementRef.nativeElement.querySelector('#s3');
+    seccion1.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  seccion4() {
+    const seccion1 = this.elementRef.nativeElement.querySelector('#s4');
+    seccion1.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  seccion5() {
+    const seccion1 = this.elementRef.nativeElement.querySelector('#s5');
+    seccion1.scrollIntoView({ behavior: 'smooth' });
+  }
 }
+
 
 document.addEventListener('click', (event: Event) => {
   const target = event.target as HTMLElement;
